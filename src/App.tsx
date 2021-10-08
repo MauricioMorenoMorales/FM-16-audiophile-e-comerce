3f394680c88d79dcc8760146ce7dc0efa61bef63
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-10-08 10:39:49 -0500

    Installation completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Text from './components/atoms/Typography';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<Text size="subTitle">Testing Size</Text>
+			<p>Testing app</p>
+		</div>
+	);
+};
 
 export default App;
